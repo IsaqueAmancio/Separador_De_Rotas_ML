bc65a4449ffb8107cf3b5c951b2f28604a298158
IsaqueAmancio <isaquealeixo@hotmail.com>
2023-11-04 00:49:18 -0300

    Add files via upload
    menu

--- calc.py
+++ calc.py
@@ -1,23 +1,26 @@
 
-lista_pct = []
-lista_pct_copia = []
-qtd_recebida = 0
 
-def add_saca(x):
-    #qtd_pcts = int(input("qtd de pacotes : "))
-    qtd_pcts = int(x)
+
+def add_saca():
+    qtd_pcts = int(input("qtd de pacotes : "))
     for i in range(qtd_pcts + 1):
         if i != 0:
             lista_pct.append(i)
     qtd_recebida = qtd_pcts
     print(qtd_recebida)
 
-def remover_parada(parada):
+def remover_parada():
+    parada = int(input("remover parada:"))
     qtd_pct = lista_pct.count(parada)
     for _ in range(qtd_pct):
         lista_pct.remove(parada)
 
-def add_pct_parada(parada,qtd):
+def add_pct_parada():
+    parada_e_qtd = str(input("digite a parada e a quantidade de pacotes: "))
+    parada_e_qtd = parada_e_qtd.split(',')
+    print(parada_e_qtd)
+    parada = int(parada_e_qtd[0])
+    qtd = int(parada_e_qtd[1])
     lista_pct.remove(parada)
     for _ in range(qtd):
         lista_pct.append(parada)
@@ -91,11 +94,29 @@ def dist_rota():
             final_rota += 1 
             for i in range(ini_rota,final_rota):
                 rota_davi.append(lista_pct[i])
-            print("------------------------")
+            print("------------------------") 
             print(rota_davi)
             print(f"PACOTES DAVI: {rota_davi.__len__()}")
         
-
+lista_pct = []
+lista_pct_copia = []
+qtd_recebida = 0
+while True:
+    print(f"--------------------------------------------------------------------------------------------------------------------------------\n                                                                                       QTD_PCTS: ???")
+    print("1 - ADICIONAR SACA")
+    print("2 - REMOVER PARADA")
+    print("3 - ADICONAR PACOTE NA PARADA")
+    print("--------------------------------------------------------------------------------------------------------------------------------")
+    menu = int(input("escolha uma opção a cima: "))
+    match menu : 
+        case 1:
+             add_saca()
+        case 2:
+            remover_parada()
+        case 3:
+            add_pct_parada()
+        case 4:
+            verificar_qtd()
 add_saca(100)
 print(lista_pct)
 remover_parada(35)
